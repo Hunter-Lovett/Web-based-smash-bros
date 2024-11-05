a772a7a62c3bc1e252cd690468d44fd2b6a78c48
Hunter-Lovett <huntellovet26@waupacaschools.org>
2024-11-05 08:24:31 -0600

    chaging some stuff

--- index.js
+++ index.js
@@ -4,5 +4,40 @@ function setup() {
 
 function draw() {
   background(220);
-  square(30, 20, 55, 20);
+  square(30, 20, 55);
 }
+//function keyPressed() {
+  if (key === 'a') {
+    // Code to run.
+  }
+  if (key === 's') {
+    // Code to run.
+  }
+  if (key === 'd') {
+    // Code to run.
+  }
+  if (key === 'w') {
+    // Code to run.
+  }
+  if (keyCode === 'q') {
+    // Code to run. specal attack 1
+  }
+  if (keyCode === 'e') {
+    // Code to run.
+  }
+  if (keyCode === '1') {
+    // Code to run. super attack
+  }
+  if (keyCode === 'arrowleft') {
+    // Code to run.
+  }
+  if (keyCode === 'arrowright') {
+    // Code to run.
+  }
+  if (keyCode === 'arrowdown') {
+    // Code to run.
+  }
+  if (keyCode === 'arrowup') {
+    // Code to run.
+  }
+//}
